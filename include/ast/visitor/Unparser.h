4cd5432fdd81c4ec528c4aef8bec2c31de04c657
Aviral Goel <goel.aviral@gmail.com>
2020-02-26 15:51:37 -0500

    Add final declaration to Unparser class and the virtual visit methods

--- include/ast/visitor/Unparser.h
+++ include/ast/visitor/Unparser.h
@@ -7,7 +7,7 @@
 
 namespace rtype::ast::visitor {
 
-class Unparser: public Visitor {
+class Unparser final: public Visitor {
   public:
     explicit Unparser(std::ostream& os): Visitor(), os_(os) {
     }
@@ -15,56 +15,56 @@ class Unparser: public Visitor {
     ~Unparser() {
     }
 
-    void visit(const rtype::ast::node::Node& node) override {
+    void visit(const rtype::ast::node::Node& node) override final {
         node.accept(*this);
     }
 
-    void visit(const rtype::ast::node::Type& node) override {
+    void visit(const rtype::ast::node::Type& node) override final {
         node.accept(*this);
     }
 
-    void visit(const rtype::ast::node::ScalarType& node) override {
+    void visit(const rtype::ast::node::ScalarType& node) override final {
         node.accept(*this);
     }
 
-    void visit(const rtype::ast::node::CompositeType& node) override {
+    void visit(const rtype::ast::node::CompositeType& node) override final {
         node.accept(*this);
     }
 
     void visit(const rtype::ast::node::SequenceType<rtype::ast::node::Type>&
-                   type) override {
+                   type) override final {
     }
 
     void visit(
         const rtype::ast::node::SequenceType<rtype::ast::node::NamedType>& node)
-        override {
+        override final {
     }
 
-    void visit(const rtype::ast::node::NamedType& node) override {
+    void visit(const rtype::ast::node::NamedType& node) override final {
         os_ << node.get_name();
         os_ << ":";
         node.get_type().accept(*this);
     }
 
-    void visit(const rtype::ast::node::CharacterType& node) override {
+    void visit(const rtype::ast::node::CharacterType& node) override final {
         os_ << "character";
     }
 
-    void visit(const rtype::ast::node::ComplexType& node) override {
+    void visit(const rtype::ast::node::ComplexType& node) override final {
         os_ << "complex";
     }
 
-    void visit(const rtype::ast::node::DoubleType& node) override {
+    void visit(const rtype::ast::node::DoubleType& node) override final {
         os_ << "double";
     }
 
-    void visit(const rtype::ast::node::FunctionType& node) override {
+    void visit(const rtype::ast::node::FunctionType& node) override final {
         node.get_parameter_types().accept(*this);
         os_ << " => ";
         node.get_return_type().accept(*this);
     }
 
-    void visit(const rtype::ast::node::ParameterType& node) override {
+    void visit(const rtype::ast::node::ParameterType& node) override final {
         os_ << "<";
         int show_separator = node.size() - 1;
         for (auto i = node.cbegin(); i != node.cend(); ++i) {
@@ -77,17 +77,17 @@ class Unparser: public Visitor {
         os_ << ">";
     }
 
-    void visit(const rtype::ast::node::GroupType& node) override {
+    void visit(const rtype::ast::node::GroupType& node) override final {
         os_ << "(";
         node.get_inner_type().accept(*this);
         os_ << ")";
     }
 
-    void visit(const rtype::ast::node::IntegerType& node) override {
+    void visit(const rtype::ast::node::IntegerType& node) override final {
         os_ << "integer";
     }
 
-    void visit(const rtype::ast::node::ListType& node) override {
+    void visit(const rtype::ast::node::ListType& node) override final {
         os_ << "list<";
         int show_separator = node.size() - 1;
         for (auto i = node.cbegin(); i != node.cend(); ++i) {
@@ -100,20 +100,20 @@ class Unparser: public Visitor {
         os_ << ">";
     }
 
-    void visit(const rtype::ast::node::LogicalType& node) override {
+    void visit(const rtype::ast::node::LogicalType& node) override final {
         os_ << "logical";
     }
 
-    void visit(const rtype::ast::node::NoNaType& node) override {
+    void visit(const rtype::ast::node::NoNaType& node) override final {
         os_ << "!";
         node.get_inner_type().accept(*this);
     }
 
-    void visit(const rtype::ast::node::RawType& node) override {
+    void visit(const rtype::ast::node::RawType& node) override final {
         os_ << "raw";
     }
 
-    void visit(const rtype::ast::node::StructType& node) override {
+    void visit(const rtype::ast::node::StructType& node) override final {
         os_ << "struct<";
         int show_separator = node.size() - 1;
         for (auto i = node.cbegin(); i != node.cend(); ++i) {
@@ -126,18 +126,18 @@ class Unparser: public Visitor {
         os_ << ">";
     }
 
-    void visit(const rtype::ast::node::UnionType& node) override {
+    void visit(const rtype::ast::node::UnionType& node) override final {
         node.get_first_type().accept(*this);
         os_ << "|";
         node.get_second_type().accept(*this);
     }
 
-    void visit(const rtype::ast::node::VectorType& node) override {
+    void visit(const rtype::ast::node::VectorType& node) override final {
         node.get_scalar_type().accept(*this);
         os_ << "[]";
     }
 
-    void visit(const rtype::ast::node::TypeDeclaration& node) override {
+    void visit(const rtype::ast::node::TypeDeclaration& node) override final {
         os_ << "type ";
         os_ << node.get_identifier() << " ";
         node.get_type().accept(*this);
