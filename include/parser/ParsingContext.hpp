2f3da5023f27e209f6075d764a561f26d2b21543
Aviral Goel <goel.aviral@gmail.com>
2020-03-15 19:26:42 -0400

    Add functionality to store token prefix in parsing context

--- include/parser/ParsingContext.hpp
+++ include/parser/ParsingContext.hpp
@@ -12,7 +12,9 @@ class ParsingContext {
   public:
     explicit ParsingContext(std::istream& input_stream,
                             const std::string& input_stream_name)
-        : input_stream_(input_stream), parse_output_(input_stream_name) {
+        : input_stream_(input_stream)
+        , parse_output_(input_stream_name)
+        , token_prefix_("") {
     }
 
     ~ParsingContext() {
@@ -26,9 +28,24 @@ class ParsingContext {
         return parse_output_;
     }
 
+    std::string& get_token_prefix() {
+        return token_prefix_;
+    }
+
+    const std::string& get_token_prefix() const {
+        return token_prefix_;
+    }
+
+    std::string extract_token_prefix() {
+        std::string result = std::move(token_prefix_);
+        token_prefix_ = "";
+        return result;
+    }
+
   private:
     std::istream& input_stream_;
     ParseResult parse_output_;
+    std::string token_prefix_;
 };
 
 } // namespace tastr::parser
